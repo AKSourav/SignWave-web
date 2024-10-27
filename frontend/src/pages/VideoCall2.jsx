d2130079fef72ecaea00de6b73850f93e2df3305
Anupam Kumar Sourav <anupam.sony32@gmail.com>
2024-10-27 13:24:45 +0530

    fix

--- frontend/src/pages/VideoCall2.jsx
+++ frontend/src/pages/VideoCall2.jsx
@@ -1,5 +1,5 @@
 import { useRef, useEffect, useState } from 'react';
-import * as myhands from '@mediapipe/hands';
+// import * as myhands from '@mediapipe/hands';
 import { drawConnectors, drawLandmarks } from '@mediapipe/drawing_utils';
 
 import CallWrapper from '../components/CallWrapper';
@@ -72,8 +72,8 @@ function VideoCall2() {
     useEffect(() => {
     
         // Dynamically import the Mediapipe Hands module
-        import('@mediapipe/hands').then(({ Hands,  HAND_CONNECTIONS}) => {
-          hands = new Hands({
+        import('@mediapipe/hands').then((myhands) => {
+          hands = new myhands.Hands({
             locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`,
           });
     
@@ -94,7 +94,7 @@ function VideoCall2() {
             if (results.multiHandLandmarks) {
               results.multiHandLandmarks.forEach((landmarks) => {
                 import('@mediapipe/drawing_utils').then(({ drawConnectors, drawLandmarks }) => {
-                  drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, { color: 'lightgreen', lineWidth: 2 });
+                  drawConnectors(canvasCtx, landmarks, myhands.HAND_CONNECTIONS, { color: 'lightgreen', lineWidth: 2 });
                   drawLandmarks(canvasCtx, landmarks, { color: 'lightgreen', radius: 1 });
                 });
               });
