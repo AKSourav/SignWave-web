9b765c1b535e090820b91e0433e943f43bd7b252
Anupam Kumar Sourav <anupam.sony32@gmail.com>
2024-10-27 14:26:12 +0530

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,17 +4,6 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['@mediapipe/hands', '@mediapipe/drawing_utils'], // Include both hands and drawing utils
-  },
-  build: {
-    rollupOptions: {
-      // Ensure external dependencies are included properly
-      external: ['@mediapipe/hands', '@mediapipe/drawing_utils'], // Marking as external to avoid bundling issues
-    },
-  },
-  resolve: {
-    alias: {
-      // Add any necessary aliases here if needed
-    },
+    include: ['@mediapipe/hands', '@mediapipe/drawing_utils'],
   },
 });
